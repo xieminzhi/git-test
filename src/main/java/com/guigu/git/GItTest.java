0de4de4c1538a050ef910f9da1a24f7ca29b09e3
Xieminzhi <xmz@163.com>
2022-10-17 11:16:24 +0800

    hot-fit 1st commit

--- src/main/java/com/guigu/git/GItTest.java
+++ src/main/java/com/guigu/git/GItTest.java
@@ -15,5 +15,7 @@ public class GItTest {
         System.out.println("hello git!");
         System.out.println("hello git2!");
         System.out.println("hello git3!");
+        System.out.println("hello git4!");
+
     }
 }
