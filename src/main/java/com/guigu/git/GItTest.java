97d26c1ab100ee7c72155e6c0cc77d8f7e0c06fc
Xieminzhi <xmz@163.com>
2022-10-17 10:53:59 +0800

    2nd commit

---
+++ src/main/java/com/guigu/git/GItTest.java
@@ -0,0 +1,18 @@
+package com.guigu.git;
+
+import static java.lang.System.*;
+
+/**
+ * @BelongsProject: git-test
+ * @BelongsPackage: com.guigu.git
+ * @Author: xieminzhi
+ * @CreateTime: 2022-10-17  10:43
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class GItTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git2!");
+    }
+}
